bc7b7b1af44ce206ea1a66a1a397946fe2066d15
Vincent <Vincent.Tsang@tufts.edu>
2016-06-20 02:51:14 -0400

    Completes indexPage and tc size()

--- javacs-lab06/src/com/flatironschool/javacs/Index.java
+++ javacs-lab06/src/com/flatironschool/javacs/Index.java
@@ -55,10 +55,14 @@ public class Index {
 	 */
 	public void indexPage(String url, Elements paragraphs) {
 		// make a TermCounter and count the terms in the paragraphs
-        // TODO: fill this in
-		
+		TermCounter counter = new TermCounter(url.toString());
+		counter.processElements(paragraphs);
+
 		// for each term in the TermCounter, add the TermCounter to the index
-        // TODO: fill this in
+		Set<String> terms = counter.keySet(); 
+		for (String term: terms){
+			add(term, counter);
+		}
 	}
 
 	/**
